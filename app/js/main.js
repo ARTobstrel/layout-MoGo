8e4f58bbeba513fe19fa1dc5b2e4b76c9f4ffe26
artem <artemfap@yandex.ru>
2019-08-21 23:52:28 +0300

    'commit4'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
+document.body.onload = function (){
+    setTimeout(function () {
+        let preloader = document.getElementById('preloader');
+        if(!preloader.classList.contains('done')){
+            preloader.classList.add('done');
+        }
+    }, 1000);
+};
+
+
+
 document.getElementById('accordion').onclick = function (event) {
     var target = event.target;
     target.getElementsByClassName('accordion__item').onmouseover = function () {
